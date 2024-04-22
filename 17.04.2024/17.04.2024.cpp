ade4e1131169eca05ce4636f5911df8aafed3893
trakt <trakt@DESKTOP-F1DCND3>
2024-04-22 17:32:19 +0300

    Добавьте файлы проекта.

---
+++ 17.04.2024/17.04.2024.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Fraction
+{
+private:
+	int m_numerator = 0;
+	int m_denominator = 1;
+
+public:
+	Fraction(int numerator = 0, int denomirator = 1) :
+		m_numerator(numerator), m_denominator(denomirator)
+	{
+	}
+
+	friend ostream& operator<<(ostream& out, const Fraction& f1)
+	{
+		out << f1.m_numerator << "/" << f1.m_denominator;
+		return out;
+	}
+};
+
+void printFraction(const unique_ptr<Fraction>& ptr)
+{
+	if (ptr)
+		cout << *ptr;
+}
+
+int main()
+{
+	unique_ptr<Fraction> ptr = make_unique<Fraction>(7, 9);
+
+	printFraction(ptr);
+}
